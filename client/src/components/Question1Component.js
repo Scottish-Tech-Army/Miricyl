47a6fad3b784e492013822108ac54f5b6150f4ed
steven <steven@burrows.plus.com>
2020-10-05 13:24:57 +0100

    changed checkboxes to buttons

--- client/src/components/Question1Component.js
+++ client/src/components/Question1Component.js
@@ -11,36 +11,25 @@ const Question1Component = (props) => {
   };
 
   const handleChange = (e) => {
-    if (e.target.checked) {
+    if (e.target.classList == "question-button") {
       tags.push(e.target.value);
-    }
-    if (!e.target.checked) {
+      e.target.classList = "question-button-selected";
+      console.log("if fired");
+    } else {
       tags.splice(tags.indexOf(e.target.value), 1);
+      e.target.classList = "question-button";
+      console.log("else fired");
     }
   };
 
-  // const setColor = (e) => {
-  //   const target = e.target,
-  //     status = e.target.classList.contains("active");
-  //   e.target.classList.add(status ? "inactive" : "active");
-  //   e.target.classList.remove(status ? "active" : "inactive");
-  // };
-
   const questions = props.questions.map((question) => (
-    <div className="question-button">
-      <input
-        type="checkbox"
-        // type="button"
-        key={question.NeedsID}
-        value={question.NeedsDesc}
-        id={question.NeedsDesc}
-        onChange={handleChange}
-        // onChange={setColor}
-        // style={color:"white"}
-        // <input
-      />
-      <label>{question.NeedsDesc}</label>
-    </div>
+    <button
+      onClick={handleChange}
+      className="question-button"
+      value={question.NeedsDesc}
+    >
+      {question.NeedsDesc}
+    </button>
   ));
 
   return (
@@ -53,15 +42,17 @@ const Question1Component = (props) => {
         <p className="question-one-text">What can we help you with?</p>
         <p className="question-one-subtext">select all that apply</p>
 
-        <form onSubmit={handleSubmit}>
-          {/* <h2>I'm looking for help with...</h2> */}
+        {/* <form onSubmit={handleSubmit}> */}
+        {/* <h2>I'm looking for help with...</h2> */}
 
-          {questions}
+        {questions}
 
-          <br />
-
-          <input type="submit" value="Next" />
-        </form>
+        <br />
+        <button className="next-button" onClick={handleSubmit}>
+          Next
+        </button>
+        {/* <input type="submit" value="Next" />
+        </form> */}
       </div>
     </div>
   );
