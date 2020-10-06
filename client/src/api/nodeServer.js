7ff3544d082cc9947561c1e5376d9ae648cf18d0
steven <steven@burrows.plus.com>
2020-10-06 13:16:13 +0100

    added conditional api lookup depending if it is in dev or production

--- client/src/api/nodeServer.js
+++ client/src/api/nodeServer.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+let development = process.env.NODE_ENV !== "production";
+
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
-  // baseURL: "https://devmiricylserver.azurewebsites.net/api",
+  baseURL: development
+    ? "http://localhost:3000/api"
+    : "https://devmiricylserver.azurewebsites.net/api",
 });
