bdc9a49deac15300be38ba1c43bdcb2890da3194
Steve Vance <steve.vance@hotmail.co.uk>
2020-10-09 16:25:50 +0100

    pulling more service data through from database

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -2,16 +2,33 @@ import React from "react";
 import "../styles/global.css";
 
 const Results = (props) => {
+
   const showResults = props.results.map((result) => (
-    <p className="results-list" key={result.PlaceID}>
-      {result.OrgName}
-    </p>
+
+    <div className="results-list-container" key={result.PlaceID}>
+        <p className="results-list-title">
+        <a href={result.ServiceURL}>{result.OrgName}</a>
+        </p>
+        <p className="results-list-service-description">{result.ServiceDescription}</p>
+          <div>
+          <button className="results-list-button" type="button">Call</button>
+          <button className="results-list-button" type="button">Chat</button>
+          <button className="results-list-button" type="button">Email</button>
+          </div>
+        <p className="results-list-address-title">Phone Support:</p>
+        <p className="results-list-address-detail">{result.OpeningTime}</p>
+        <p className="results-list-address-title">Address:</p>
+        <p className="results-list-address-detail">{result.PhysicalAddress}</p>
+        <p className="results-list-address-title">About</p>
+        <p className="results-list-address-detail">{result.OrgDescription}</p>
+      
+    </div>
   ));
 
   return (
     <div className="results-container">
       <h1 className="question-title">Search results</h1>
-      <h4 className="results-list-name">{showResults}</h4>
+      {showResults}
     </div>
   );
 };
