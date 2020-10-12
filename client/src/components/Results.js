075703ac1086999b2b5cd2e1261f76232e79c98b
Steve Vance <steve.vance@hotmail.co.uk>
2020-10-12 13:18:16 +0100

    added conditional around ServiceURL

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -7,19 +7,35 @@ const Results = (props) => {
 
     <div className="results-list-container" key={result.PlaceID}>
         <div className="results-title-container">
-        <img className="results-list-logo" src={result.Logo}/>
-        <div className="results-list-title-service">
+          <img className="results-list-logo" src={result.Logo}/>
+          <div className="results-list-title-service">
           <p className="results-list-title"> 
-        <a href={result.ServiceURL}>{result.OrgName}</a>
-        </p>
-        <p className="results-list-service-description">{result.ServiceDescription}
-        </p>
-        </div>
+        
+          {result.ServiceURL ? <a href={result.ServiceURL}>{result.OrgName}</a> : <p>{result.OrgName}</p>}
+          </p>
+          
+          <p className="results-list-service-description">{result.ServiceDescription}</p>
+          </div>
         </div>
           <div>
           <button className="results-list-button" type="button">Call</button>
           <button className="results-list-button" type="button">Chat</button>
-          <button className="results-list-button" type="button">Email</button>
+          <button className="results-list-button" type="button">
+
+            {result.EmailAddress &&  <a class="mailto" href={result.EmailAddress}>Email</a>}
+          
+          {/* {result.EmailAddress ? <a class="mailto" href={result.EmailAddress}>Email</a> : <button className="results-list-button" type="button">Email</button>} */}
+          
+          {/* {result.EmailAddress ? <a class="mailto" href={result.EmailAddress}>Email</a> : null} */}
+          {/* <a class="mailto" href={result.EmailAddress}>Email</a> */}
+          </button>
+          
+          
+          
+          
+          {/* <p className="results-list-email">{result.EmailAddress}</p> */}
+
+          
           </div>
         <p className="results-list-address-title">Phone Support:</p>
         <p className="results-list-address-detail">{result.OpeningTime}</p>
