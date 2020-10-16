c5a028becd9ddc20e0def31a2481048ce64e5a57
a8ree <aidan@thebrees.co.uk>
2020-10-16 08:52:25 +0100

    remove old commentary

--- devops/provider.tf
+++ devops/provider.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {   
     version         = "=2.28.0"
-    # Free trial
     subscription_id = "__subscriptionid__"
     client_id       = "__clientid__"
     client_secret   = "__clientsecret__"
