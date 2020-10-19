975089f937e99a18d146bce6e36fa7d8d9be4289
Steve Vance <steve.vance@hotmail.co.uk>
2020-10-19 09:25:52 +0100

    button icons complete

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,43 +1,54 @@
 import React from "react";
 import "../styles/global.css";
+import {IconContext} from "react-icons";
+import { BiPhone } from "react-icons/bi";
+import { BiEnvelope } from "react-icons/bi";
+import { BiChat } from "react-icons/bi";
+import { BiMap } from "react-icons/bi";
+
 
 const Results = (props) => {
 
   const showResults = props.results.map((result) => (
 
-    <div className="results-list-container" key={result.PlaceID}>
+    <div className="results-list-container" key={ result.PlaceID }>
         <div className="results-title-container">
-          <img className="results-list-logo" src={result.Logo}/>
+          <img className="results-list-logo" src={ result.Logo }/>
           <div className="results-list-title-service">
           
           {result.ServiceURL ? 
-          <p className="results-list-title"> <a href={result.ServiceURL}>{result.OrgName}</a></p> : 
-          <p className="results-list-title">{result.OrgName}</p>}
+          <p className="results-list-title"> <a href={ result.ServiceURL }>{ result.OrgName }</a></p> : 
+          <p className="results-list-title">{ result.OrgName }</p>}
         
-          <p className="results-list-service-description">{result.ServiceDescription}</p>
+          <p className="results-list-service-description">{ result.ServiceDescription }</p>
           </div>
         </div>
 
-        <div>  
-          {result.PhoneNo ? 
-          <button className="results-list-button" type="button">
-            <a href={"tel:" + result.PhoneNo}>Call</a>
-          </button> : null }
+        <div className="results-button-container">  
+          { result.PhoneNo ? 
+          <button className="results-list-button" type="button"><IconContext.Provider value={{ className: 'results-react-button-icon' }}>
+            <a href={ "tel:" + result.PhoneNo }><BiPhone /> Call</a>
+            </IconContext.Provider></button> : null }
 
-          <button className="results-list-button" type="button">Chat</button>
+          {result.ServiceURL ? 
+          <button className="results-list-button" type="button">
+            <IconContext.Provider value={{ className: 'results-react-button-icon' }}>
+            <a href={ result.ServiceURL }>
+          <BiChat /> Chat </a>
+          </IconContext.Provider></button> : null }
           
           {result.EmailAddress ? 
-          <button className="results-list-button" type="button">
-            <a class="mailto" href={"mailto:" + result.EmailAddress}>Email</a>
-          </button> : null} 
+          <button className="results-list-button" type="button"><IconContext.Provider value={{ className: 'results-react-button-icon' }}>
+            <a class="mailto" href={"mailto:" + result.EmailAddress}><BiEnvelope /> Email</a>
+            </IconContext.Provider></button> : null} 
         </div>
 
         {result.OpeningTime ? 
-        <p className="results-list-sub-title">Phone Support:</p> : null }
+        <p className="results-list-sub-title"><IconContext.Provider value={{ className: 'results-react-icons-detail' }}> <BiPhone /> Phone Support:</IconContext.Provider></p> : null }
         <p className="results-list-service-detail">{result.OpeningTime}</p>
         
         {result.PhysicalAddress ? 
-        <p className="results-list-sub-title">Address:</p>  : null }
+        <p className="results-list-sub-title"><IconContext.Provider value={{ className: 'results-react-icons-detail' }}> <BiMap /> Address:</IconContext.Provider></p> : null }
         <p className="results-list-service-detail">{result.PhysicalAddress}</p>
         
 
@@ -46,7 +57,8 @@ const Results = (props) => {
 
         {result.TypeOfSupport ? 
         <p className="results-list-sub-title">Types of Service:</p> : null }
-        <li className="results-list-service-detail">{result.TypeOfSupport}</li>
+        {result.TypeOfSupport ? 
+        <li className="results-list-service-detail">{result.TypeOfSupport}</li>: null}
         
         {result.OrgDescription ? 
         <p className="results-list-sub-title">About</p> : null }
