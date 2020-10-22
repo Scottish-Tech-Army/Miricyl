eb5093f1670081dc218448415a345ac704c8db55
Steve Vance <steve.vance@hotmail.co.uk>
2020-10-22 14:48:02 +0100

    added NavBar component to file structure

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "../styles/global.css";
+
+const NavBar = () => {
+
+    return (
+        <h1>This is the Nav Bar with Logo and help button</h1>
+    )
+};
+
+export default withRouter(NavBar);
