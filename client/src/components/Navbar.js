87505effffe76d2d8a46ca840003214e8f7f03ff
Steve Vance <steve.vance@hotmail.co.uk>
2020-10-22 16:39:30 +0100

    navbar logo added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/global.css";
+import Logo from "../assets/BoostLogo.png";
 
 const NavBar = () => {
 
     return (
         <>
         <div className="navbar-container">
-            <h1 className="nav-logo">LOGO HERE</h1>
+            {/* <h1 className="nav-logo">LOGO HERE</h1> */}
+            <img className="navbar-logo" src={Logo} alt="logo"/>
             <button className="help-button">Help!</button>
 
         </div>
