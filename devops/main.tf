3e84cd8a4c33b761b9f7ea26482f6b0aa19f0a23
a8ree <aidan@thebrees.co.uk>
2020-11-04 15:13:02 +0000

    typo fix

--- devops/main.tf
+++ devops/main.tf
@@ -136,8 +136,8 @@ resource "azurerm_app_service" "testing" {
 
 resource "azurerm_app_service_custom_hostname_binding" "customdomains" {
   for_each            = lookup(local.environments, local.custom_domain)
-  hostname            = ${each.value}
-  app_service_name    = azurerm_app_service.${each.key}.name
+  hostname            = "${each.value}"
+  app_service_name    = "azurerm_app_service.${each.key}.name"
   resource_group_name = azurerm_resource_group.primary_webapp.name
 }
 
