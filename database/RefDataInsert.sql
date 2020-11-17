ace196c4c2e2639ff20e4baa8e8b98baabfbe7b1
steven <steven@burrows.plus.com>
2020-11-17 21:47:04 +0000

    fixed a bug

--- database/RefDataInsert.sql
+++ database/RefDataInsert.sql
@@ -1,99 +1,133 @@
-Insert into Miricyl.Country values(101,'Scotland');
-Insert into Miricyl.Country values(102,'England');
-Insert into Miricyl.Country values(103,'Wales');
-Insert into Miricyl.Country values(104,'Northen Ireland');
-Insert into Miricyl.Country values(105,'United Kingdom');
-Insert into Miricyl.Gender values(101,'Female');
-Insert into Miricyl.Gender values(102,'Male');
-Insert into Miricyl.Gender values(103,'Both');
+Insert into Miricyl.Country
+values(101, 'Scotland');
+Insert into Miricyl.Country
+values(102, 'England');
+Insert into Miricyl.Country
+values(103, 'Wales');
+Insert into Miricyl.Country
+values(104, 'Northen Ireland');
+Insert into Miricyl.Country
+values(105, 'United Kingdom');
+Insert into Miricyl.Gender
+values(101, 'Female');
+Insert into Miricyl.Gender
+values(102, 'Male');
+Insert into Miricyl.Gender
+values(103, 'Both');
 
-<<<<<<< HEAD
-Insert into Miricyl.Needs Values(101,'Abuse','Abuse or Bullying');
-=======
-Insert into Miricyl.Needs Values(101,'Abuse','Childhood abuse or Bullying');
->>>>>>> develop
-Insert into Miricyl.Needs Values(102,'Addiction','Addiction');
-Insert into Miricyl.Needs Values(103,'Alcohol','Addiction');
-Insert into Miricyl.Needs Values(104,'Anger','Anxiety, Anger or Stress');
-Insert into Miricyl.Needs Values(105,'Anxiety','Anxiety, Anger or Stress');
-<<<<<<< HEAD
-Insert into Miricyl.Needs Values(106,'Bereavement','');
-Insert into Miricyl.Needs Values(107,'Body image','Body image or Eating disorders');
-Insert into Miricyl.Needs Values(108,'Bullying','Abuse or Bullying');
-Insert into Miricyl.Needs Values(109,'Care','');
-Insert into Miricyl.Needs Values(110,'Debt','Debt or Unemployment');
-Insert into Miricyl.Needs Values(111,'Depression','Feeling low or Depressed');
-Insert into Miricyl.Needs Values(112,'Discrimination','');
-Insert into Miricyl.Needs Values(113,'Domestic Abuse','Domestic Abuse');
-Insert into Miricyl.Needs Values(114,'Drugs','Addiction');
-Insert into Miricyl.Needs Values(115,'Eating disorders','Body image or Eating disorders');
-Insert into Miricyl.Needs Values(116,'Feeling low','Feeling low or Depressed');
-Insert into Miricyl.Needs Values(117,'Gender identity','LGBTQ+ or Gender identity');
-Insert into Miricyl.Needs Values(118,'General Mental Health','');
-Insert into Miricyl.Needs Values(119,'Into work /unemployment','Debt or Unemployment');
-Insert into Miricyl.Needs Values(120,'Involved in crime','Crime');
-Insert into Miricyl.Needs Values(121,'LGBTQ+','LGBTQ+ or Gender identity');
-Insert into Miricyl.Needs Values(122,'Loneliness','');
-Insert into Miricyl.Needs Values(123,'Money','Debt or Unemployment');
-Insert into Miricyl.Needs Values(124,'New parent','Pregnancy and Parenting');
-Insert into Miricyl.Needs Values(125,'Panic Attacks','Anxiety, Anger or Stress');
-Insert into Miricyl.Needs Values(126,'Parenting','Pregnancy and Parenting');
-Insert into Miricyl.Needs Values(127,'Pregnant','Pregnancy and Parenting');
-Insert into Miricyl.Needs Values(128,'Ralationship issue','');
-=======
-Insert into Miricyl.Needs Values(106,'Bereavement','Relationship or Bereavement');
-Insert into Miricyl.Needs Values(107,'Body image','Body image or Eating disorders');
-Insert into Miricyl.Needs Values(108,'Bullying','Childhood abuse or Bullying');
-Insert into Miricyl.Needs Values(109,'I am caring for someone else','ZA-Future Need');
-Insert into Miricyl.Needs Values(110,'Debt','Debt or Looking for Employment');
-Insert into Miricyl.Needs Values(111,'Depression','Low, Depressed or Lonely');
-Insert into Miricyl.Needs Values(112,'Discrimination','ZA-Future Need');
-Insert into Miricyl.Needs Values(113,'Domestic Abuse','Domestic abuse or Sexual violence');
-Insert into Miricyl.Needs Values(114,'Drugs','Addiction');
-Insert into Miricyl.Needs Values(115,'Eating disorders','Body image or Eating disorders');
-Insert into Miricyl.Needs Values(116,'Feeling low','Low, Depressed or Lonely');
-Insert into Miricyl.Needs Values(117,'Gender identity','ZA-Future Need');
-Insert into Miricyl.Needs Values(118,'General Mental Health','ZA-Future Need');
-Insert into Miricyl.Needs Values(119,'Into work /unemployment','Debt or Looking for Employment');
-Insert into Miricyl.Needs Values(120,'Involved in crime','Victim or Involved in Crime');
-Insert into Miricyl.Needs Values(121,'I was in care(foster/home)','ZA-Future Need');
-Insert into Miricyl.Needs Values(122,'Loneliness','Low, Depressed or Lonely');
-Insert into Miricyl.Needs Values(123,'Money','Debt or Looking for Employment');
-Insert into Miricyl.Needs Values(124,'New parent','Parenting');
-Insert into Miricyl.Needs Values(125,'Panic Attacks','Anxiety, Anger or Stress');
-Insert into Miricyl.Needs Values(126,'Parenting','Parenting');
-Insert into Miricyl.Needs Values(127,'Pregnant','Parenting');
-Insert into Miricyl.Needs Values(128,'Ralationship issue','Relationship or Bereavement');
->>>>>>> develop
-Insert into Miricyl.Needs Values(129,'Self harm','Suicide or Self-harm');
-Insert into Miricyl.Needs Values(130,'Sleep','Sleeping problems');
-Insert into Miricyl.Needs Values(131,'Stress','Anxiety, Anger or Stress');
-Insert into Miricyl.Needs Values(132,'Suicide','Suicide or Self-harm');
-Insert into Miricyl.Needs Values(133,'Victim of crime','Victim or Involved in Crime');
+Insert into Miricyl.Needs
+Values(101, 'Abuse', 'Childhood abuse or Bullying');
+Insert into Miricyl.Needs
+Values(102, 'Addiction', 'Addiction');
+Insert into Miricyl.Needs
+Values(103, 'Alcohol', 'Addiction');
+Insert into Miricyl.Needs
+Values(104, 'Anger', 'Anxiety, Anger or Stress');
+Insert into Miricyl.Needs
+Values(105, 'Anxiety', 'Anxiety, Anger or Stress');
+Insert into Miricyl.Needs
+Values(106, 'Bereavement', 'Relationship or Bereavement');
+Insert into Miricyl.Needs
+Values(107, 'Body image', 'Body image or Eating disorders');
+Insert into Miricyl.Needs
+Values(108, 'Bullying', 'Childhood abuse or Bullying');
+Insert into Miricyl.Needs
+Values(109, 'I am caring for someone else', 'ZA-Future Need');
+Insert into Miricyl.Needs
+Values(110, 'Debt', 'Debt or Looking for Employment');
+Insert into Miricyl.Needs
+Values(111, 'Depression', 'Low, Depressed or Lonely');
+Insert into Miricyl.Needs
+Values(112, 'Discrimination', 'ZA-Future Need');
+Insert into Miricyl.Needs
+Values(113, 'Domestic Abuse', 'Domestic abuse or Sexual violence');
+Insert into Miricyl.Needs
+Values(114, 'Drugs', 'Addiction');
+Insert into Miricyl.Needs
+Values(115, 'Eating disorders', 'Body image or Eating disorders');
+Insert into Miricyl.Needs
+Values(116, 'Feeling low', 'Low, Depressed or Lonely');
+Insert into Miricyl.Needs
+Values(117, 'Gender identity', 'ZA-Future Need');
+Insert into Miricyl.Needs
+Values(118, 'General Mental Health', 'ZA-Future Need');
+Insert into Miricyl.Needs
+Values(119, 'Into work /unemployment', 'Debt or Looking for Employment');
+Insert into Miricyl.Needs
+Values(120, 'Involved in crime', 'Victim or Involved in Crime');
+Insert into Miricyl.Needs
+Values(121, 'I was in care(foster/home)', 'ZA-Future Need');
+Insert into Miricyl.Needs
+Values(122, 'Loneliness', 'Low, Depressed or Lonely');
+Insert into Miricyl.Needs
+Values(123, 'Money', 'Debt or Looking for Employment');
+Insert into Miricyl.Needs
+Values(124, 'New parent', 'Parenting');
+Insert into Miricyl.Needs
+Values(125, 'Panic Attacks', 'Anxiety, Anger or Stress');
+Insert into Miricyl.Needs
+Values(126, 'Parenting', 'Parenting');
+Insert into Miricyl.Needs
+Values(127, 'Pregnant', 'Parenting');
+Insert into Miricyl.Needs
+Values(128, 'Ralationship issue', 'Relationship or Bereavement');
+Insert into Miricyl.Needs
+Values(129, 'Self harm', 'Suicide or Self-harm');
+Insert into Miricyl.Needs
+Values(130, 'Sleep', 'Sleeping problems');
+Insert into Miricyl.Needs
+Values(131, 'Stress', 'Anxiety, Anger or Stress');
+Insert into Miricyl.Needs
+Values(132, 'Suicide', 'Suicide or Self-harm');
+Insert into Miricyl.Needs
+Values(133, 'Victim of crime', 'Victim or Involved in Crime');
 
-Insert into Miricyl.Type Values(101,'Information','Information');
-Insert into Miricyl.Type Values(102,'Directory of local services','Directory of local services');
-Insert into Miricyl.Type Values(103,'Face to Face Counselling','Face to face');
-Insert into Miricyl.Type Values(104,'Free services','Free services');
-Insert into Miricyl.Type Values(105,'Drop in','Face to face');
-Insert into Miricyl.Type Values(106,'In a Group','In a Group');	
-Insert into Miricyl.Type Values(107,'One to One','Face to face');
-Insert into Miricyl.Type Values(108,'Self Help','Information');
-Insert into Miricyl.Type Values(109,'Online support - Message Boards/Forums','Email or Forum');
-Insert into Miricyl.Type Values(110,'Online support by Telephone','Telephone or Zoom');
-Insert into Miricyl.Type Values(111,'Online support by Chat','Chat or Text');
-Insert into Miricyl.Type Values(112,'Online support by Text','Chat or Text');
-Insert into Miricyl.Type Values(113,'Online support by Email','Email or Forum');
-Insert into Miricyl.Type Values(114,'Online support by Video','Telephone or Zoom');
+Insert into Miricyl.Type
+Values(101, 'Information', 'Information');
+Insert into Miricyl.Type
+Values(102, 'Directory of local services', 'Directory of local services');
+Insert into Miricyl.Type
+Values(103, 'Face to Face Counselling', 'Face to face');
+Insert into Miricyl.Type
+Values(104, 'Free services', 'Free services');
+Insert into Miricyl.Type
+Values(105, 'Drop in', 'Face to face');
+Insert into Miricyl.Type
+Values(106, 'In a Group', 'In a Group');
+Insert into Miricyl.Type
+Values(107, 'One to One', 'Face to face');
+Insert into Miricyl.Type
+Values(108, 'Self Help', 'Information');
+Insert into Miricyl.Type
+Values(109, 'Online support - Message Boards/Forums', 'Email or Forum');
+Insert into Miricyl.Type
+Values(110, 'Online support by Telephone', 'Telephone or Zoom');
+Insert into Miricyl.Type
+Values(111, 'Online support by Chat', 'Chat or Text');
+Insert into Miricyl.Type
+Values(112, 'Online support by Text', 'Chat or Text');
+Insert into Miricyl.Type
+Values(113, 'Online support by Email', 'Email or Forum');
+Insert into Miricyl.Type
+Values(114, 'Online support by Video', 'Telephone or Zoom');
 
-Insert into Miricyl.Personalisation Values(101,'Under 16','Under 18');
-Insert into Miricyl.Personalisation Values(102,'16+','');
-Insert into Miricyl.Personalisation Values(103,'18+','');
-Insert into Miricyl.Personalisation Values(104,'A student','A student');
-Insert into Miricyl.Personalisation Values(105,'A parent','A parent');
-Insert into Miricyl.Personalisation Values(106,'LGBTQ+','LGBTQ+');
-Insert into Miricyl.Personalisation Values(107,'Unemployed','Unemployed');
-Insert into Miricyl.Personalisation Values(108,'A carer','A carer');
-Insert into Miricyl.Personalisation Values(109,'BAME','BAME');
+Insert into Miricyl.Personalisation
+Values(101, 'Under 16', 'Under 18');
+Insert into Miricyl.Personalisation
+Values(102, '16+', '');
+Insert into Miricyl.Personalisation
+Values(103, '18+', '');
+Insert into Miricyl.Personalisation
+Values(104, 'A student', 'A student');
+Insert into Miricyl.Personalisation
+Values(105, 'A parent', 'A parent');
+Insert into Miricyl.Personalisation
+Values(106, 'LGBTQ+', 'LGBTQ+');
+Insert into Miricyl.Personalisation
+Values(107, 'Unemployed', 'Unemployed');
+Insert into Miricyl.Personalisation
+Values(108, 'A carer', 'A carer');
+Insert into Miricyl.Personalisation
+Values(109, 'BAME', 'BAME');
 
 
