903d7c968b1811dd4ac77768fabd2b9c039d01b4
steven <steven@burrows.plus.com>
2020-12-31 15:50:32 +0000

    fixed sonarcloud security on server showing http header

--- server/server.js
+++ server/server.js
@@ -4,10 +4,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.disable("x-powered-by");
 app.use("/api", apiRouter);
 app.use(cors());
 
 app.listen("3000", () => {
-  console.log("server is running", process.env.NODE_ENV);
 });
