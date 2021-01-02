ef9ad26627c0a8050849bb2813d2d67168ec4dc2
steven <steven@burrows.plus.com>
2021-01-02 19:55:37 +0000

    fixed sonar cloud cors security bug

--- server/server.js
+++ server/server.js
@@ -5,8 +5,24 @@ const app = express();
 
 app.use(express.json());
 app.disable("x-powered-by");
+
+var allowedDomains = ['*.miricyl.org', 'http://localhost:3001'];
+app.use(cors({
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+
+    if (allowedDomains.indexOf(origin) === -1) {
+      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// app.use(cors());
 app.use("/api", apiRouter);
-app.use(cors());
+
 
 app.listen("3000", () => {
 });
