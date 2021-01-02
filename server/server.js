46e8022e3368670ae5196decf5651887e993a8c7
steven <steven@burrows.plus.com>
2021-01-02 15:02:36 +0000

    server bug fix

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.disable("x-powered-by");
-app.use("/api", apiRouter);
 app.use(cors());
+app.use("/api", apiRouter);
+
 
 app.listen("3000", () => {
   console.log("server is running", process.env.NODE_ENV);
