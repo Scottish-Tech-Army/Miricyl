3ecd5f0b30a748ffa1c64d5150da48bf8ce3cced
michael <michaellaidlawscott96@gmail.com>
2021-01-09 17:14:29 +0000

    finished cards with some placeholder values

--- client/src/components/OrgCard/OrgCard.jsx
+++ client/src/components/OrgCard/OrgCard.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 import * as styles from "./orgCard.module.scss";
+import { IconContext } from "react-icons";
+import { BiPhone } from "react-icons/bi";
+import { BiEnvelope } from "react-icons/bi";
+import { BiChat } from "react-icons/bi";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
 import ReactStars from "react-rating-stars-component";
 
 const OrgCard = ({ charity }) => {
-  console.log(charity);
+  const [isExapnded, setIsExpanded] = useState(false);
+
   return (
     <div className={styles.container}>
       <OrgOverview charity={charity} />
       <OptionsMet services={charity.Services} />
       <div className={styles.servicesContainer}>
-        {charity.Services.map((service) => (
+        {charity.Services.slice(
+          0,
+          isExapnded ? charity.Services.length : 2
+        ).map((service) => (
           <Service service={service} />
         ))}
       </div>
-      <div className={styles.orgDetailsContainer}>Org Details</div>
+      <OrgDetails
+        email={charity.Email}
+        phone={charity.PhoneNo}
+        address={charity.Address}
+      />
+      <button
+        className={styles.expandButtonContainer}
+        onClick={() => {
+          setIsExpanded(!isExapnded);
+        }}
+        disabled={charity.Services.length <= 2}
+      >
+        {isExapnded ? (
+          <MdKeyboardArrowDown className={`${styles.expandIcon}`} />
+        ) : (
+          <MdKeyboardArrowUp
+            className={`${styles.expandIcon} ${
+              charity.Services.length <= 2 && styles.disabled
+            }`}
+          />
+        )}
+      </button>
     </div>
   );
 };
@@ -42,14 +73,6 @@ const OrgOverview = ({ charity }) => {
 };
 
 const OptionsMet = ({ services }) => {
-  // const matchedPersonalisations = [
-  //   ...new Set(
-  //     service.Personalisations.filter((personalisation) =>
-  //       selectedPersonalisations.includes(personalisation)
-  //     )
-  //   ),
-  // ];
-
   const uniqueMatchedNeeds = [
     ...new Set(...services.map((service) => service.matchedNeeds)),
   ];
@@ -87,7 +110,64 @@ const Service = ({ service }) => {
       <span className={styles.serviceInfo}>{service.PhoneNo}</span>
       <span className={styles.serviceInfo}>{service.OpeningTimes}</span>
       <span className={styles.serviceInfo}>{service.PhysicalAddress}</span>
-      <div className={styles.serviceContactBoxes}>boxes</div>
+      <div className={styles.serviceContactBoxes}>
+        {service.PhoneNo && (
+          <button className="results-list-button" type="button">
+            <IconContext.Provider
+              value={{ className: "results-react-button-icon" }}
+            >
+              <a href={"tel:" + service.PhoneNo}>
+                <BiPhone /> Call
+              </a>
+            </IconContext.Provider>
+          </button>
+        )}
+
+        {service.ServiceUrl && (
+          <button className="results-list-button" type="button">
+            <IconContext.Provider
+              value={{ className: "results-react-button-icon" }}
+            >
+              <a href={service.ServiceUrl} target="_blank">
+                <BiChat /> Chat{" "}
+              </a>
+            </IconContext.Provider>
+          </button>
+        )}
+
+        {service.EmailAddress && (
+          <button className="results-list-button" type="button">
+            <IconContext.Provider
+              value={{ className: "results-react-button-icon" }}
+            >
+              <a class="mailto" href={"mailto:" + service.EmailAddress}>
+                <BiEnvelope /> Email
+              </a>
+            </IconContext.Provider>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const OrgDetails = ({ phone, email, address }) => {
+  return (
+    <div className={styles.orgDetailsContainer}>
+      <span className={styles.orgDetailsTitle}>
+        Organisation contact details:
+      </span>
+      <div className={styles.contactDetailsContainer}>
+        <span>
+          <b>Phone:</b> {email ?? "placeholder"}
+        </span>
+        <span>
+          <b>Email:</b> {phone ?? "placeholder"}
+        </span>
+        <span>
+          <b>Address:</b> {address ?? "placeholder"}
+        </span>
+      </div>
     </div>
   );
 };
