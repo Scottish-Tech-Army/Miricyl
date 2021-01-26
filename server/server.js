8a511b11a3e8708446d07297335c357d3018365a
steven <steven@burrows.plus.com>
2021-01-26 21:04:22 +0000

    fixed server cors and added footer

--- server/server.js
+++ server/server.js
@@ -9,9 +9,17 @@ let development = process.env.NODE_ENV == "development";
 
 var corsOptions = {}
 
-corsOptions = {
-  origin: 'https://__env__.miricyl.org',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+if (development) {
+  corsOptions = {
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+} else {
+  corsOptions = {
+    origin: 'https://__env__.miricyl.org',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 }
 
 
