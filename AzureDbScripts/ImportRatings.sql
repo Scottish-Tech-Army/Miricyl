4b2fe7b5643e29eced7da7eb7192d33703bf9b18
a8ree <aidan@thebrees.co.uk>
2021-02-05 17:05:01 +0000

    Update ImportRatings.sql

--- AzureDbScripts/ImportRatings.sql
+++ AzureDbScripts/ImportRatings.sql
@@ -1,5 +1,5 @@
  use `__dbname__`;
- LOAD DATA   infile 'extracted/Ratings.csv' into table `__dbname__`.RatingImport
+ LOAD DATA LOCAL INFILE 'extracted/Ratings.csv' into table `__dbname__`.RatingImport
   FIELDS TERMINATED BY ','
   Enclosed by '"'
  IGNORE 1 LINES
