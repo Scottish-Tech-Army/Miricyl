4f54934f03149f2a5dce7e1ac89521ad9133f320
Aidan <aidan@abitsolutions.co.uk>
2021-02-13 21:06:41 +0000

    .

--- devops/main.tf
+++ devops/main.tf
@@ -630,7 +630,7 @@ resource "azurerm_key_vault_access_policy" "appgateway" {
 # This retrieves the IDs of all the certificates in the vault
 data "azurerm_key_vault_certificate" "miricyl" {
 for_each       = lookup(local.environments, local.zone)
-  name         = "dev-np-miricyl-org"
+  name         = "${each.value}-np-miricyl-org"
   key_vault_id = azurerm_key_vault.appgateway.id
 }
 
