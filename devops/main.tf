8f2594e0a64da94f042d6db10a6f99893e2b0e70
Aidan <aidan@abitsolutions.co.uk>
2021-02-13 21:13:44 +0000

    .

--- devops/main.tf
+++ devops/main.tf
@@ -753,7 +753,7 @@ frontend_ip_configuration {
     frontend_ip_configuration_name = "${azurerm_public_ip.appgateway.name}-ipconfig"
     frontend_port_name             = "${local.prefix}-${local.primary_location}-feport"
     protocol                       = "https"
-    host_name                      = "${http_listener.value}.${local.prefix}.org"
+    host_name                      = "${http_listener.value}.${local.zone}.${local.prefix}.org"
     ssl_certificate_name           = "${http_listener.value}-miricyl-org"
   }
  }
