ea4f0859854e39ba1f1db7a29cffcb65c0a04878
Aidan <aidan@abitsolutions.co.uk>
2021-02-13 21:14:33 +0000

    .

--- devops/main.tf
+++ devops/main.tf
@@ -778,7 +778,7 @@ frontend_ip_configuration {
     frontend_ip_configuration_name  = "${azurerm_public_ip.appgateway.name}-ipconfig"
     frontend_port_name              = "${local.prefix}-${local.primary_location}-feport-http"
     protocol                        = "http"
-    host_name                       = "${http_listener.value}.${local.prefix}.org"
+    host_name                       = "${http_listener.value}.${local.zone}.${local.prefix}.org"
   }
  }
 /* --remove
